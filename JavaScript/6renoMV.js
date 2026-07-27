66bc1138cef8d256d4a38f0c38e81b5ba6dbe0d0
Xuan-Xuan12 <TANYX13.26@ichat.sp.edu.sg>
2026-07-27 22:44:59 +0800

    Add files via upload

---
+++ JavaScript/6renoMV.js
@@ -0,0 +1,17 @@
+function toggleBTS(){
+
+    let section = document.getElementById("bts");
+
+    if(section.style.display === "none"){
+
+        section.style.display = "block";
+
+    }
+
+    else{
+
+        section.style.display = "none";
+
+    }
+
+}
